90a6eb0e31591fe07681e7bdc77a06ac22586197
ChloeJu <lj1015@nyu.edu>
2019-03-27 06:18:57 -0400

    Update Water Tile&&Add bubble tea creature tile

--- Assets/Resources/Chloe/Water.cs
+++ Assets/Resources/Chloe/Water.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Water : Tile
 {
+
+	float f = 0;
+	public bool startW = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void OnTriggerStay2D(Collider2D otherCollider) {
+		Tile maybeTile = otherCollider.GetComponent<Tile>();
+		if (maybeTile != null ) {
+
+
+			Player py =maybeTile.GetComponent<Player> ();
+			py.moveSpeed += 30;
+			startW=true;
+
+			//py.moveSpeed = -py.moveSpeed;
+		}
+	}
+	void OnTriggerExit2D(Collider2D otherCollider) {
+		Tile maybeTile = otherCollider.GetComponent<Tile>();
+		if (maybeTile != null ) {
+
+
+			Player py =maybeTile.GetComponent<Player> ();
+			py.moveSpeed = 10;
+			startW=true;
+
+			//py.moveSpeed = -py.moveSpeed;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update()
+
+
+	{
+		
+}
 }

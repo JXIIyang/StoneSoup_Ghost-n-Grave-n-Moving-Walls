3abed5c44e45fe4afa43b63f21fc1ec3a3061233
Jiaxi Yang <jy2134@nyu.edu>
2019-03-27 01:34:47 -0400

    add Statue tile.

---
+++ Assets/Resources/Eleanor/Scripts/PrayScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrayScript : MonoBehaviour
+{
+    public SpriteRenderer Sprite;
+    public List<Tile> Enemies = new List<Tile>();
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (Sprite.color.a < 0.7f) return;
+        Tile enemyTile = other.GetComponent<Tile>();
+        if (enemyTile.hasTag(TileTags.Enemy) && !Enemies.Contains(enemyTile))
+        {
+            Enemies.Add(enemyTile);
+        }
+    }
+}
